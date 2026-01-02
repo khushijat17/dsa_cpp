4a3366ec9e79a6bbe9bc9712aac4a52be59d3fb6
khushijat17 <harekrishna.khushijat17@gmail.com>
2026-01-02 21:37:54 +0530

    Create even.cpp

---
+++ odd/even.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+void fun(int n){
+    if(n%2==0){
+        cout<<"even";
+      }  else{
+        cout<<"odd";
+    }
+        
+    
+}
+int main(){
+    fun(29);
+    return 0;
+}
+ 
+
